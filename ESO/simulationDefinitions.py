9369ea8f6e4632b207c4cfaaaa6b2e7a3e974f6d
JenniferKarr <jkarr@asiaa.sinica.edu.tw>
2024-04-09 14:42:38 +0800

    updated recipes.yaml with various sources  - darks, detlin, flat  - single sources in imaging, spectra, IFU  - star field in imaging LM  - extended galaxy in LM
    updated simulationDefinitions.py
      - single sources
      - star field
      - extended galaxy
    
    recipes1.yaml: still in progress templatesx

--- ESO/simulationDefinitions.py
+++ ESO/simulationDefinitions.py
@@ -1,7 +1,28 @@
 import astropy.units as u
 import scopesim as sim
 import scopesim_templates as sim_tp
+import numpy as np
 
+
+# use the same random star field each time
+
+starFieldX = np.array([-8.15592743,  7.01303926,  8.01500244,  1.87226377,  6.97505972,
+       -7.33994824,  0.04191974,  5.35931242,  8.40940718, -0.49102622,
+        4.58550425,  6.10882803, -1.99466201, -9.72891962, -3.65611485,
+       -1.20411157, -2.02697232,  8.42325234, -5.67781285,  8.68952776])
+
+starFieldY = np.array([ 9.583468  , -5.65889576,  7.44908775,  4.17753575,  4.43878784,
+        1.18114661,  5.65337934, -6.90408802, -0.49683094,  6.04866284,
+        8.58989225,  8.85721093,  0.7475543 , -1.90119023,  4.98409528,
+       -0.96123847,  9.34819477,  9.42408694,  8.20907011, -1.03093753])
+
+
+starFieldM = np.array([13.9583468 , 12.43411042, 13.74490878, 13.41775357, 13.44387878,
+       13.11811466, 13.56533793, 12.3095912 , 12.95031691, 13.60486628,
+       13.85898923, 13.88572109, 13.07475543, 12.80988098, 13.49840953,
+       12.90387615, 13.93481948, 13.94240869, 13.82090701, 12.89690625])*u.mag
+
+starFieldT = ["A0V","A0V","A0V","A0V","A0V","A0V","A0V","A0V","A0V","A0V","A0V","A0V","A0V","A0V","A0V","A0V","A0V","A0V","A0V","A0V"]
 
 SOURCEDICT = {
     "empty_sky": (sim_tp.empty_sky, {}),
@@ -14,6 +35,33 @@ SOURCEDICT = {
             "extend": 15,
         }
     ),
+    "star_field_rand":(
+        sim_tp.stellar.star_field,
+        {
+            "n":20,
+            "mmin":10*u.mag,
+            "mmax":12*u.mag,
+            "width":20,
+            "filter_name":"Ks",
+        }),
+
+    "star_field":(
+        sim_tp.stellar.stars,
+        {
+            "amplitudes":starFieldM,
+            "x":starFieldX,
+            "y":starFieldY,
+            "filter_name":"Ks",
+            "spec_types":starFieldT,
+        }),
+
+    
+    "simple_star18": (
+        sim_tp.stellar.star,
+        {
+            "filter_name":"V",
+            "amplitude": [18]*u.mag,
+        }),
     "simple_star12": (
         sim_tp.stellar.star,
         {
