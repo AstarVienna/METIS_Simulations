55f0a0886c88d6e3de7ddabc4697b71ad16f787e
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2024-11-02 11:18:56 +0100

    Add some output and exit on failure

--- ESO/check_sof_files.py
+++ ESO/check_sof_files.py
@@ -6,6 +6,8 @@ PATH_HERE = Path(__file__).parent
 PATH_SOFS = PATH_HERE / "sofFiles"
 PATH_FITS = PATH_HERE / "output"
 
+problems = []
+
 for filename in PATH_SOFS.glob("*.sof"):
     # print(filename)
     lines = open(filename, mode="r", encoding="utf-8").readlines()
@@ -20,6 +22,12 @@ for filename in PATH_SOFS.glob("*.sof"):
         if not (PATH_FITS / fn).exists()
     ]
     if fns_raw_missing:
-        print(filename)
-        print(fns_raw_missing)
+        problems.append((filename.name, fns_raw_missing))
 
+if problems:
+    print("Some RAW files are missing:")
+    for fn_sof, fns_missing in problems:
+        print("-", fn_sof)
+        for fn in fns_missing:
+            print("  -", fn)
+    exit(1)
