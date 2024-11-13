2251bc6997c9e4bd50a3d30712d0e928776a9efc
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2024-11-13 14:59:34 +0100

    Check whether the contents of the SOF files matches the DRLD

--- ESO/check_sof_files.py
+++ ESO/check_sof_files.py
@@ -1,20 +1,56 @@
 #!/usr/bin/env python
+"""Check SOF files.
 
+First install the DRLD with
+METIS_DRLD> pip install -e .
+
+But that does not always work (TODO: fix), so you can also specify the
+Python path:
+export PYTHONPATH=/path/to/METIS_DRLD/codes
+"""
 from pathlib import Path
+from codes.drld_parser.data_reduction_library_design import METIS_DataReductionLibraryDesign
+
 
 PATH_HERE = Path(__file__).parent
 PATH_SOFS = PATH_HERE / "sofFiles"
 PATH_FITS = PATH_HERE / "output"
 
-problems = []
+problems_raws = []
+problems_tags = []
+problems_names = []
 
 for filename in PATH_SOFS.glob("*.sof"):
     # print(filename)
-    lines = open(filename, mode="r", encoding="utf-8").readlines()
+    lines1 = open(filename, mode="r", encoding="utf-8").readlines()
+    lines = [
+        [a.split("/")[-1] for a in line.split()]
+        for line in lines1
+    ]
+
+    # Check whether tags are in the DRLD.
+    tags = [tag for _, tag in lines]
+    tags_missing = [
+        tag for tag in tags
+        if tag not in METIS_DataReductionLibraryDesign.dataitems
+    ]
+    if tags_missing:
+        problems_tags.append((filename.name, tags_missing))
+
+    # Check whether the tag is in the filename.
+    filenames_without_tags = [
+        fn
+        for fn, tag in lines
+        if tag not in fn
+    ]
+    if filenames_without_tags:
+        problems_names.append((filename.name, filenames_without_tags))
+
+    # Check whether raws exist.
     fns_raw = [
-        line.split()[0].split("/")[1]
-        for line in lines
-        if "RAW" in line
+        fn
+        for fn, tag in lines
+        if "RAW" in tag
     ]
     fns_raw_missing = [
         fn
@@ -22,12 +58,31 @@ for filename in PATH_SOFS.glob("*.sof"):
         if not (PATH_FITS / fn).exists()
     ]
     if fns_raw_missing:
-        problems.append((filename.name, fns_raw_missing))
+        problems_raws.append((filename.name, fns_raw_missing))
 
-if problems:
+if problems_raws:
     print("Some RAW files are missing:")
-    for fn_sof, fns_missing in problems:
+    for fn_sof, fns_missing in problems_raws:
         print("-", fn_sof)
         for fn in fns_missing:
             print("  -", fn)
+
+print()
+if problems_tags:
+    print("Some tags are not in the DRLD:")
+    for fn_sof, fns_missing in problems_tags:
+        print("-", fn_sof)
+        for fn in fns_missing:
+            print("  -", fn)
+
+print()
+if problems_names:
+    print("Some filenames do not contain the tag::")
+    for fn_sof, fns_missing in problems_names:
+        print("-", fn_sof)
+        for fn in fns_missing:
+            print("  -", fn)
+
+
+if problems_raws or problems_tags:
     exit(1)
