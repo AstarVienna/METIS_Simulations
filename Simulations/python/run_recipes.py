dcede5b26d48d1c832e52df1749874d83265bb8f
eiseleb47 <benjamin.eisele0101@gmail.com>
2025-06-17 15:33:05 +0200

    Renamed irdb_path and removed dublicated code

--- Simulations/python/run_recipes.py
+++ Simulations/python/run_recipes.py
@@ -10,17 +10,17 @@ import sys
 import makeCalibPrototypes
 from pathlib import Path
 
-def runRecipes(inputYAML=Path.joinpath(Path(__file__).parent.parent, 
-                                       "YAML/allRecipes.yaml"), 
+def runRecipes_with_pars(inputYAML=Path(__file__).parent.parent / "YAML" / "allRecipes.yaml", 
                                        outputDir="output/", 
                                        small=False, 
-                                       doStatic=False, 
-                                       doCalib=0, 
-                                       sequence="1", 
+                                       doStatic=True, 
+                                       catglist=None,
+                                       doCalib=1, 
+                                       sequence='1', 
                                        testRun=False, 
                                        calibFile=None, 
                                        nCores=8, 
-                                       scopesim_path=Path.home() / ".sigmas_pkg", 
+                                       irdb_path='./inst_pks', 
                                        **kwargs):
     """
     Run a set of recipes with explicit arguments instead of command line.
@@ -37,11 +37,12 @@ def runRecipes(inputYAML=Path.joinpath(Path(__file__).parent.parent,
 
     # Set parameters directly
     simulationSet.setParms(
-        scopesim_path=scopesim_path,
+        irdb_path=irdb_path,
         inputYAML=inputYAML,
         outputDir=outputDir,
         small=small,
         doStatic=doStatic,
+        catglist=catglist,
         doCalib=doCalib,
         sequence=sequence,
         testRun=testRun,
@@ -86,35 +87,11 @@ def runRecipes(inputYAML=Path.joinpath(Path(__file__).parent.parent,
 
     if simulationSet.params['doStatic']:
         makeCalibPrototypes.generateStaticCalibs(simulationSet.params['outputDir'])
+
+def runRecpies_with_argv(argv):
+    simulationSet = rr.runRecipes()
+    simulationSet.parseCommandLine(argv[1:])
+    runRecipes_with_pars(**simulationSet.params)
         
 if __name__ == "__main__":
-    simulationSet = rr.runRecipes()
-    simulationSet.parseCommandLine(sys.argv[1:])
-
-    simulationSet.loadYAML()
-    
-    goodInput = simulationSet.validateYAML()
-
-    if (not goodInput):
-        exit
-
-    if(simulationSet.params['doCalib'] > 0):
-        simulationSet.calculateCalibs()
-
-    simulationSet.runSimulations()
-    
-    if(simulationSet.params['doCalib'] > 0):
-        simulationSet.runCalibrations()
-
-        if(simulationSet.params['calibFile'] is not None):
-            simulationSet.dumpCalibsToFile()
-
-    simulationSet.allFileNames.sort()
-    for elem in simulationSet.allFileNames:
-        print(elem)
-
-    if(not simulationSet.params['testRun']):
-        simulationSet.updateHeaders()
-
-    if(simulationSet.params['doStatic']):
-        makeCalibPrototypes.generateStaticCalibs(simulationSet.params['outputDir'])
+    runRecpies_with_argv(sys.argv)
