eb3bbd3b427c90533e34e86fbd37802e4d10eb31
JenniferKarr <jkarr@asiaa.sinica.edu.tw>
2025-10-08 15:47:58 +0800

    matching

--- Simulations/runESOsmall.sh
+++ Simulations/runESOsmall.sh
@@ -1,12 +1,12 @@
 
 # run the simulation blocks for all instruments
 
-python/imgLM_small.py
-python/imgN_small.py
-python/lssLM_small.py
-python/lssN_small.py
-python/ifu_small.py
-python/calib_small.py
-python/hciRavcLM_small.py
-python/hciAppLM_small.py
-python/hciRavcIFU_small.py
+python python/imgLM_small.py
+python python/imgN_small.py
+python python/lssLM_small.py
+python python/lssN_small.py
+python python/ifu_small.py
+python python/calib_small.py
+python python/hciRavcLM_small.py
+python python/hciAppLM_small.py
+python python/hciRavcIFU_small.py
