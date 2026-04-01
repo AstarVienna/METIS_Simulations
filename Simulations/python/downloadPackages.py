4038f4abc4ec702f8ff0ac94032172a6995db2eb
JenniferKarr <jkarr@asiaa.sinica.edu.tw>
2026-04-01 13:33:14 +0800

    removed excess files

--- Simulations/python/downloadPackages.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import scopesim as sim
-
-sim.download_packages("METIS")
-sim.download_packages("Armazones")
-sim.download_packages("ELT")
--- Simulations/sofTemplates/incorrect/README.md
+++
@@ -1,2 +0,0 @@
-These files are intentionally malformed or otherwise incorrect.
-They are to be only used for automated testing. Do not delete.
