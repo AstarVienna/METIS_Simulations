be2e18300ca3352c9db27383a145253d8463df11
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2026-04-19 21:25:13 +0200

    Run a smaller set to test

--- runESO.sh
+++ runESO.sh
@@ -1,10 +1,10 @@
 
 python simulationBlocks/imgLM.py
-python simulationBlocks/imgN.py
-python simulationBlocks/lssLM.py
-python simulationBlocks/lssN.py
-python simulationBlocks/ifu.py
-python simulationBlocks/calib.py
-python simulationBlocks/hciRavcLM.py
-python simulationBlocks/hciAppLm.py
-python simulationBlocks/hciRavcIfu.py
+#python simulationBlocks/imgN.py
+#python simulationBlocks/lssLM.py
+#python simulationBlocks/lssN.py
+#python simulationBlocks/ifu.py
+#python simulationBlocks/calib.py
+#python simulationBlocks/hciRavcLM.py
+#python simulationBlocks/hciAppLm.py
+#python simulationBlocks/hciRavcIfu.py
